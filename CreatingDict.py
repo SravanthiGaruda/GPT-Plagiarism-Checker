1f4e5194d113147caff2c1efdef0305beb4e8989
Sravanthi Garuda <sravanthigaruda@Sravanthis-Air.attlocal.net>
2022-12-29 01:06:19 -0800

    Create Dict for Questions and Answers

---
+++ CreatingDict.py
@@ -0,0 +1,29 @@
+import itertools
+
+def CreatingDict():
+
+    dict_Questions_Answers = {}
+    questions = []
+    answers = []
+    with open('TestCases2.txt') as file:
+        for line in file:
+            print(line)
+            if not line:
+                continue
+            elif 'Question' in line:
+                questions.append(line)
+    
+    with open('TestCases2.txt') as file:
+        for line in file:
+            print(line)
+            if not line:
+                continue
+            elif 'Answer' in line:
+                answers.append(line)
+
+    for (question, ans) in itertools.zip_longest(questions, answers):
+        dict_Questions_Answers[question] = ans
+
+    print(dict_Questions_Answers)
+
+CreatingDict()
